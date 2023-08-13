d33dda6ca52361fddc861dbdaf0574a123d1b4d6
alijr2018 <alirami120@hotmail.fr>
2023-08-13 17:19:04 +0100

    done

--- models/base_model.py
+++ models/base_model.py
@@ -5,21 +5,27 @@ Write a class BaseModel
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    x = '%Y-%m-%dT%H:%M:%S.%f'
+                    setattr(self, key, datetime.strptime(value, x))
                 elif key != '__class__':
                     setattr(self, key, value)
+
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        class_name = self.__class__.__name__
+        instance_id = self.id
+        instance_dict = self.__dict__
+        return ("[{}] ({}) {}".format(class_name, instance_id, instance_dict))
 
     def save(self):
         self.updated_at = datetime.now()
