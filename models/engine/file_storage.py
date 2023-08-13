5b63e947fb2824ec2057ce3e07451edaed21a203
alijr2018 <alirami120@hotmail.fr>
2023-08-13 21:31:52 +0100

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,12 +6,6 @@ Defines a `FileStorage` class.
 import os
 import json
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.review import Review
-from models.amenity import Amenity
-from models.place import Place
 
 
 class FileStorage():
@@ -44,23 +38,12 @@ class FileStorage():
                 {k: v.to_dict() for k, v in FileStorage.__objects.items()}, f)
 
     def reload(self):
-        """
-        deserializes the JSON file to __objects only if the JSON
-        file exists; otherwise, does nothing
-        """
-        current_classes = {'BaseModel': BaseModel, 'User': User,
-                           'Amenity': Amenity, 'City': City, 'State': State,
-                           'Place': Place, 'Review': Review}
-        if not os.path.exists(FileStorage.__file_path):
-            return
-        with open(FileStorage.__file_path, 'r') as f:
-            deserialized = None
-            try:
-                deserialized = json.load(f)
-            except json.JSONDecodeError:
-                pass
-            if deserialized is None:
-                return
-            FileStorage.__objects = {
-                k: current_classes[k.split('.')[0]](**v)
-                for k, v in deserialized.items()}
+        """Deserialize/convert obj dicts back to instances, if it exists"""
+        try:
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                new_obj_dict = json.load(f)
+            for key, value in new_obj_dict.items():
+                obj = self.class_dict[value['__class__']](**value)
+                self.__objects[key] = obj
+        except FileNotFoundError:
+            pass
