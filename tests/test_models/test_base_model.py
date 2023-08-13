f548755b07a8c0bd98ffd2c0ce2d0bfc252646ab
alijr2018 <alirami120@hotmail.fr>
2023-08-13 17:30:04 +0100

    done

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""Defines unittests for models/base_model.py.
+Unittest classes:
+    TestBaseModel_instantiation
+    TestBase_Instance_Print
+    TestBaseModel_save
+    TestBase_from_json_string
+    TestBaseModel_to_dict
+"""
 import sys
 import os
 
