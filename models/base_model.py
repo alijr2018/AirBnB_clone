78f3a9f89f71c54890930beeeaec857509dda137
alijr2018 <alirami120@hotmail.fr>
2023-08-13 17:22:48 +0100

    done

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,7 @@ from datetime import datetime
 
 
 class BaseModel:
+    """  that defines all common attributes/methods for other classes."""
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
@@ -28,11 +29,13 @@ class BaseModel:
         return ("[{}] ({}) {}".format(class_name, instance_id, instance_dict))
 
     def save(self):
+        """updates the public instance attribute with the current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """returns a dictionary containing all keys/values of the instance."""
         new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__class__.__name__
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
-        return new_dict
+        return (new_dict)
