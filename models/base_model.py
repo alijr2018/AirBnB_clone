de36df76d4d105b7d5b398e5c8e5c74e023a7772
alijr2018 <alirami120@hotmail.fr>
2023-08-14 00:31:35 +0100

    done

--- models/base_model.py
+++ models/base_model.py
@@ -30,10 +30,18 @@ class BaseModel():
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
+        """
+        updates the public instance attribute updated_at with,
+        the current datetime.
+        """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
+        """
+        returns a dictionary containing all keys/values,
+        of __dict__ of the instance.
+        """
         new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__class__.__name__
         new_dict['created_at'] = self.created_at.isoformat()
