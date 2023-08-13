fc07f4d5afc26ebccfa8183aea084716f3906eb9
alijr2018 <alirami120@hotmail.fr>
2023-08-13 17:53:24 +0100

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,67 +1,37 @@
 #!/usr/bin/python3
 
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+from datetime import datetime
 
 
 class FileStorage:
-    """Handles serialization & deserialization of objects to/from JSON files"""
-
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Returns the dictionary of objects."""
-        return (FileStorage.__objects)
+        return self.__objects
 
     def new(self, obj):
-        """Sets the object in the dictionary."""
-        key = obj.__class__.__name__ + "." + obj.id
-        FileStorage.__objects[key] = obj
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to JSON file."""
-        new_dict = {}
-        for key, value in FileStorage.__objects.items():
-            new_dict[key] = value.to_dict()
-        with open(FileStorage.__file_path, 'w') as file:
-            json.dump(new_dict, file)
+        serialized = {}
+        for key, value in self.__objects.items():
+            serialized[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding="utf-8") as file:
+            json.dump(serialized, file)
 
     def reload(self):
-        """Deserializes the JSON file to __objects."""
         try:
-            with open(FileStorage.__file_path, "r") as file:
-                data = json.load(file)
-                for key, value in data.items():
+            with open(self.__file_path, 'r', encoding="utf-8") as file:
+                loaded_objs = json.load(file)
+                for key, value in loaded_objs.items():
                     class_name = value['__class__']
-                    if class_name == 'User':
-                        obj = User(**value)
-                    else:
-                        obj = BaseModel(**value)
-                    FileStorage.__objects[key] = obj
-        except Exception:
+                    del value['__class__']
+                    value['created_at'] = datetime.strptime(value['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                    value['updated_at'] = datetime.strptime(value['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                    new_obj = globals()[class_name](**value)
+                    self.__objects[key] = new_obj
+        except FileNotFoundError:
             pass
-
-    def _deserialize_objects(self):
-        classes = {
-            "BaseModel": BaseModel,
-            "State": State,
-            "City": City,
-            "Amenity": Amenity,
-            "Place": Place,
-            "Review": Review
-        }
-
-        if os.path.exists(self.__file_path):
-            with open(self.__file_path, "r", encoding="utf-8") as file:
-                data = json.load(file)
-                for key, value in data.items():
-                    cls_name = value['__class__']
-                    if cls_name in classes:
-                        self.__objects[key] = classes[cls_name](**value)
