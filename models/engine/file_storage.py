9d6c71ca496dc550db9d54ca3763b01e060b52a2
alijr2018 <alirami120@hotmail.fr>
2023-08-13 22:07:14 +0100

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -40,21 +40,23 @@ class FileStorage():
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """Save/serialize obj dictionaries to json file"""
-        obj_dict = {}
-
-        for key, obj in self.__objects.items():
-            obj_dict[key] = obj.to_dict()
-        with open(self.__file_path, 'w', encoding="UTF-8") as f:
-            json.dump(obj_dict, f)
+        """serializes __objects to the JSON file (path: __file_path)"""
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(
+                {k: v.to_dict() for k, v in FileStorage.__objects.items()}, f)
 
     def reload(self):
-        """Deserialize/convert obj dicts back to instances, if it exists"""
-        try:
-            with open(self.__file_path, 'r', encoding="UTF-8") as f:
-                new_obj_dict = json.load(f)
-            for key, value in new_obj_dict.items():
-                obj = self.class_dict[value['__class__']](**value)
-                self.__objects[key] = obj
-        except FileNotFoundError:
-            pass
+        """deserializes the JSON file to __objects if the JSON file exists"""
+        if not os.path.exists(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, 'r') as f:
+            deserialized = None
+            try:
+                deserialized = json.load(f)
+            except json.JSONDecodeError:
+                pass
+            if deserialized is None:
+                return
+            FileStorage.__objects = {
+                k: current_classes[k.split('.')[0]](**v)
+                for k, v in deserialized.items()}
