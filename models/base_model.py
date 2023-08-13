a798a87979fe67cd18c6dac9c3dad09f49ae840a
alijr2018 <alirami120@hotmail.fr>
2023-08-13 23:12:56 +0100

    done

--- models/base_model.py
+++ models/base_model.py
@@ -13,20 +13,18 @@ class BaseModel():
     """
 
     def __init__(self, *args, **kwargs):
-
-        if len(kwargs) > 0:
+        if kwargs:
             for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.fromisoformat(value)
-                setattr(self, key, value)
-            return
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        models.storage.new(self)
+                if key == 'created_at' or key == 'updated_at':
+                    x = '%Y-%m-%dT%H:%M:%S.%f'
+                    setattr(self, key, datetime.strptime(value, x))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
 
