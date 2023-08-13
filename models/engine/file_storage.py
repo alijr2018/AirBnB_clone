fe86140290ee9323f18b7ab3826b95654c60b2d7
alijr2018 <alirami120@hotmail.fr>
2023-08-13 21:29:38 +0100

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """
 Module: file_storage.py
-
 Defines a `FileStorage` class.
 """
 import os
 import json
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.review import Review
+from models.amenity import Amenity
+from models.place import Place
 
 
 class FileStorage():
@@ -14,7 +19,6 @@ class FileStorage():
     serializes instances to a JSON file and
     deserializes JSON file to instances
     """
-
     __file_path = "file.json"
     __objects = {}
 
@@ -47,21 +51,16 @@ class FileStorage():
         current_classes = {'BaseModel': BaseModel, 'User': User,
                            'Amenity': Amenity, 'City': City, 'State': State,
                            'Place': Place, 'Review': Review}
-
         if not os.path.exists(FileStorage.__file_path):
             return
-
         with open(FileStorage.__file_path, 'r') as f:
             deserialized = None
-
             try:
                 deserialized = json.load(f)
             except json.JSONDecodeError:
                 pass
-
             if deserialized is None:
                 return
-
             FileStorage.__objects = {
                 k: current_classes[k.split('.')[0]](**v)
                 for k, v in deserialized.items()}
