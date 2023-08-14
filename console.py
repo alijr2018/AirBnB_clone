79dc351745e9b635be26ba890a4e11d7ed9771b3
alijr2018 <alirami120@hotmail.fr>
2023-08-14 18:20:46 +0100

    done

--- console.py
+++ console.py
@@ -6,6 +6,8 @@ Module: console.py
 """
 
 import cmd
+import re
+from shlex import split
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
@@ -135,6 +137,23 @@ class HBNBCommand(cmd.Cmd):
             else:
                 setattr(storage.all()[new_str], arr[2], arr[3])
                 storage.save()
+
+    def parse(arg):
+        curly_braces = re.search(r"\{(.*?)\}", arg)
+        brackets = re.search(r"\[(.*?)\]", arg)
+        if curly_braces is None:
+            if brackets is None:
+                return [i.strip(",") for i in split(arg)]
+            else:
+                lexer = split(arg[:brackets.span()[0]])
+                retl = [i.strip(",") for i in lexer]
+                retl.append(brackets.group())
+                return (retl)
+        else:
+            lexer = split(arg[:curly_braces.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(curly_braces.group())
+            return (retl)
 
 
 if __name__ == '__main__':
